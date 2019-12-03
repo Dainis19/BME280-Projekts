b10ad8e47552f6880e53ac51f7e77714566713bd
Dainis19 <48949357+Dainis19@users.noreply.github.com>
2019-12-03 08:17:29 +0200

    Update hun.py

--- hun.py
+++ hun.py
@@ -9,9 +9,7 @@ bus = smbus2.SMBus(port)
 
 calibration_params = bme280.load_calibration_params(bus, address)
 
-
 data = bme280.sample(bus, address, calibration_params)
-
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
